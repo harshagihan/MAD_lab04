d730ba8b29b0f71f7ad24565e7908984d68396aa
Harsha Gihan Madhushankha Liyanage <57912882+harshagihan@users.noreply.github.com>
2020-03-27 22:46:37 +0530

    first commit

---
+++ app/src/main/java/com/example/mad_lab_04/Registration.java
@@ -0,0 +1,14 @@
+package com.example.mad_lab_04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
